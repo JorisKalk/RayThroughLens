dfd819914ce084da610b12d9e6e5e2662d702646
Joris <549108@student.saxion.nl>
2026-04-26 17:11:25 +0200

    started on scripts

--- RayThroughLens/Assets/Scripts/LensScripts/LensMain.cs
+++ RayThroughLens/Assets/Scripts/LensScripts/LensMain.cs
@@ -2,6 +2,31 @@ using UnityEngine;
 
 public class LensMain : MonoBehaviour
 {
+    [Header("Refractive indices")]
+    [SerializeField]
+    private float airIndex = 0f;
+    [SerializeField]
+    private float lensIndex = 1.5f;
+
+    [Header("Radiuses of lens sides (min 3)")]
+    [SerializeField]
+    private float leftSideRadius = 5f;
+    [SerializeField]
+    private float rightSideRadius = 5f;
+
+    enum LensType
+    {
+        Converging,
+        Diverging
+    };
+
+    [Header("Converging or Diverging")]
+    [SerializeField]
+    private LensType leftLensType = new LensType();
+    [SerializeField]
+    private LensType rightLensType = new LensType();
+    
+
     void Start()
     {
         
@@ -9,6 +34,33 @@ public class LensMain : MonoBehaviour
 
     void Update()
     {
-        
+        if (leftSideRadius < 3f) leftSideRadius = 3f;
+        if (rightSideRadius < 3f) rightSideRadius = 3f;
+    }
+
+    public float LeftSideImpactAngle(Vector3 impactPoint, Vector3 impactDirection)
+    {
+        Vector3 lensSphereCenter = Vector3.zero;
+        if (leftLensType == LensType.Converging)
+        {
+
+        }
+
+        float temp = 0f;
+        return temp;
+    }
+
+    public float RightSideImpactAngle(Vector3 impactPoint, Vector3 impactDirection)
+    {
+
+
+        float temp = 0f;
+        return temp;
+    }
+
+    public float CalculateAngle(Vector3 directionOne, Vector3 directionTwo)
+    {
+        float temp = 0f;
+        return temp;
     }
 }
